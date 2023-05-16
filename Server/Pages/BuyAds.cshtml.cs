46cee6db68b2a2d5a7eb35bb5c3ac38142d1d171
gustavkristoferlundberg <gustav.k.lundberg@gmail.com>
2023-05-16 14:10:19 +0200

    SellAd, BuyAd, SeeYourAds

--- Server/Pages/BuyAds.cshtml.cs
+++ Server/Pages/BuyAds.cshtml.cs
@@ -1,11 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Server.Data;
+using Server.Models;
 
-namespace Server.Pages;
 
-public class BuyAds : PageModel
+namespace Server.Pages
 {
-    public void OnGet()
+    public class BuyAds : PageModel
     {
-        
+        public List<BuyAd> AllBuyAds { get; set; }
+        public DatabaseContext Context { get; set; }
+
+        public BuyAds(DatabaseContext context)
+        {
+            Context = context;
+            AllBuyAds = new List<BuyAd>();
+        }
+
+        public void OnGet()
+        {
+            AllBuyAds = Context.BuyAds.ToList();
+        }
     }
 }
--- Server/Pages/SellAds.cshtml.cs
+++ Server/Pages/SellAds.cshtml.cs
@@ -3,26 +3,28 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Server.Data;
 using Server.Models;
 
+
 namespace Server.Pages
 {
     public class SellAds : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+      
         public List<User> Users { get; set; }
+        public List<SellAd> AllSellAds { get; set; }
         public DatabaseContext Context { get; set; }
         [BindProperty]
         public User TheUser { get; set; }
 
-        public SellAds(ILogger<IndexModel> logger, DatabaseContext context)
+        public SellAds(DatabaseContext context)
         {
-            _logger = logger;
             Context = context;
             Users = new List<User>();
+            AllSellAds = new List<SellAd>();
         }
 
         public void OnGet()
         {
-            Users = Context.Users.ToList();
+            AllSellAds = Context.SellAds.ToList();
         }
     }
 }
