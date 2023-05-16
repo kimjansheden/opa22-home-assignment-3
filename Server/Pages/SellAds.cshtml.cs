86ed5b554913c4f3bbb9b28775dcf44d4debdab9
gustavkristoferlundberg <gustav.k.lundberg@gmail.com>
2023-05-16 10:15:27 +0200

    Update SellAds.cshtml.cs

--- Server/Pages/SellAds.cshtml.cs
+++ Server/Pages/SellAds.cshtml.cs
@@ -1,11 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Server.Data;
+using Server.Models;
 
-namespace Server.Pages;
-
-public class SellAds : PageModel
+namespace Server.Pages
 {
-    public void OnGet()
+    public class SellAds : PageModel
     {
-        
+        private readonly ILogger<IndexModel> _logger;
+        public List<User> Users { get; set; }
+        public DatabaseContext Context { get; set; }
+        [BindProperty]
+        public User TheUser { get; set; }
+
+        public SellAds(ILogger<IndexModel> logger, DatabaseContext context)
+        {
+            _logger = logger;
+            Context = context;
+            Users = new List<User>();
+        }
+
+        public void OnGet()
+        {
+            Users = Context.Users.ToList();
+        }
     }
 }
